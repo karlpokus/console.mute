72a1befe198db2146f76fb86b4ce872bff0e219c
OmniJeff <jeffrey.white@me.com>
2016-08-09 16:57:12 -0400

    Mute stderr too.

--- tests.js
+++ tests.js
@@ -13,19 +13,31 @@ test('mute and resume', function(t){
   console.log('log');
   console.log('muted');
   var muted = console.resume('preserve');
-  t.equal(muted.join(), 'log,muted', 'muted data returned on resume');
+  t.equal(muted.stdout.join(), 'log,muted', 'muted stdout data returned on resume');
   
   console.mute();
   console.log('moar');
   console.log('data');
   var history = console.resume();
-  t.equal(history.join(), 'log,muted,moar,data', 'history preserved');
+  t.equal(history.stdout.join(), 'log,muted,moar,data', 'history preserved');
 
   console.mute();
   console.log('muted');
   console.log('again');
   var reset = console.resume();
-  t.equal(reset.join(), 'muted,again', 'data reset is default');
+  t.equal(reset.stdout.join(), 'muted,again', 'data reset is default');
+  
+  console.mute();
+  console.error('oh');
+  console.error('noes');
+  var mutedErr = console.resume('preserve');
+  t.equal(mutedErr.stderr.join(), 'oh,noes', 'muted stderr data returned on resume');
+  
+  console.mute();
+  console.error('dis');
+  console.error('bad');
+  var errHistory = console.resume();
+  t.equal(errHistory.stderr.join(), 'oh,noes,dis,bad', 'history preserved');
   
   t.end();
 });
